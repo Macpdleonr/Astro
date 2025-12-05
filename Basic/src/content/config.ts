99fc8dff813de047b2e4c7cbe693c0c41b2f36fa
MacleonDev <macpdleonr@gmail.com>
2025-12-04 23:41:54 -0500

    Update config.ts

--- Basic/src/content/config.ts
+++ Basic/src/content/config.ts
@@ -6,12 +6,14 @@ const books = defineCollection({
   schema: z.object({
     title: z.string(),
     author: z.string(),
-    img: z.string().url(),
+    img: z.string(),
     readtime: z.number(),
     description: z.string(),
     buy: z.object({
-      spain: z.string(),
-      usa: z.string(),
+      spain: z.string().url(),
+      usa: z.string().url(),
     }),
   })
 })
+
+export const collections = {books}
