845406c663ae19da03998c2c68f940a7d252a3d7
MacleonDev <macpdleonr@gmail.com>
2025-12-09 01:38:38 -0500

    Update config.ts

--- Basic/src/content/config.ts
+++ Basic/src/content/config.ts
@@ -10,7 +10,7 @@ const books = defineCollection({
     readtime: z.number(),
     description: z.string(),
     buy: z.object({
-      spain: z.string().url(),
+      // spain: z.string().url(),
       usa: z.string().url(),
     }),
   })
