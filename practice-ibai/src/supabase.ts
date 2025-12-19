4890a247ed19e23396d727cd2831404c7885c5b0
MacleonDev <macpdleonr@gmail.com>
2025-12-19 01:12:23 -0500

    Update supabase.ts

--- practice-ibai/src/supabase.ts
+++ practice-ibai/src/supabase.ts
@@ -2,4 +2,5 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.BASE_URL
 const supabaseKey = import.meta.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey); 
+
+export const supabase = createClient(supabaseUrl, supabaseKey); 
