ae66821092dc4d231627f0eb452bdffe7b7faa13
MacleonDev <macpdleonr@gmail.com>
2025-12-18 23:33:25 -0500

    Update confetti.ts

---
+++ practice-ibai/src/utils/confetti.ts
@@ -0,0 +1,20 @@
+import confetti from 'canvas-confetti';
+
+export function throwConfetti() {
+
+  confetti({
+    colors: ['#fb9b134'],
+    particleCount: 100,
+    spread: 100,
+    angle: 60,
+    origin:{ x: 0, y: 1 }
+  });
+
+  confetti({
+    colors: ['#fb9b134'],
+    particleCount: 100,
+    spread: 100,
+    angle: 120,
+    origin: { x: 1, y: 1 }
+  });
+}
