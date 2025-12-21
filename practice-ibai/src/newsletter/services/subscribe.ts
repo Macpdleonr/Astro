1f4ad4a27c806a51d42fbdd855d5e999da7340f2
MacleonDev <macpdleonr@gmail.com>
2025-12-20 23:44:09 -0500

    Update subscribe.ts

--- practice-ibai/src/newsletter/services/subscribe.ts
+++ practice-ibai/src/newsletter/services/subscribe.ts
@@ -1,20 +1,32 @@
 import { supabase } from "@/supabase"
 
+const ERROR_CODE_ALREADY_EXISTS = '23505'
+
 export const saveNewsLetterEmail = async (email: string) => {
 
   const {data, error } = await supabase.from('newsletter').insert({email})
 
+  if (error?.code === ERROR_CODE_ALREADY_EXISTS) {
+    return {
+      duplicated: true,
+      success: true,
+      message: "Email already subscribed"
+    }
+  }
+
   if (error) {
 
     console.error(error)
-    
+
     return {
+      duplicated: false,
       success: false,
       message: "Error saving email"
     }
   }
 
   return {
+    duplicated: false,
     success: true,
     error: null
   }
