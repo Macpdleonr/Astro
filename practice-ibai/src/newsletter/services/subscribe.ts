8074cdf616a04743ebd905ebed65039b70d6d66d
Mac <71369817+Macpdleonr@users.noreply.github.com>
2025-12-24 12:54:30 -0500

    Add mock return for saveNewsLetterEmail function
    Added a mock return object for the saveNewsLetterEmail function to handle cases without a database connection.

--- practice-ibai/src/newsletter/services/subscribe.ts
+++ practice-ibai/src/newsletter/services/subscribe.ts
@@ -3,7 +3,15 @@ import { supabase } from "@/supabase"
 const ERROR_CODE_ALREADY_EXISTS = '23505'
 
 export const saveNewsLetterEmail = async (email: string) => {
+  
+  // For Works without bd
+  return {
+    duplicated: false,
+    success: true,
+    error: null
+  }
 
+  // This code never works bc we need to api to supabase
   const { error } = await supabase.from('newsletter').insert({email})
 
   if (error?.code === ERROR_CODE_ALREADY_EXISTS) {
